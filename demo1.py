17d4b745b9cc620035221e46a2f29d0bde7912c6
dan <1123414@qq.com>
2018-06-23 13:55:27 +0800

    new code

---
+++ demo1.py
@@ -0,0 +1,21 @@
+#1.注释：代码中不会被编译执行的部分（不会起到任何程序作用，用来备注用的）
+#在说明性文字前加#键可以单行注释
+'''
+'''
+"""
+A.对程序进行说明备注  B.关闭程序中的某项功能
+"""
+#建议：写程序多写注释
+
+print("HELLO world") ;print("hello python")       #每条语句结束后可以没有分号r如果一行要写多条语句，那么每条语句之间用分号隔开
+
+# print("hello world")
+print("hello world")
+# print("hello world")
+print('dandan')
+# print('hahahahahaha')\
+list1 = [2,4,6]
+num = [3*x for x in list1]
+print(num)
+
+
